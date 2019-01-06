07d72c62eca838c2a6e35f4cbea7ccfae112033c
adm.kle <adm.kle@local.thepillclub.com>
2019-01-05 20:08:53 -0800

    feat: ChangePassword At Next Login
    - Add changePasswordAtNextLogin to ps script
    - Add temp_pw to spreadsheet
    
    This allows users to set users to change pw at next login in the
    spreadsheet.

--- bulkaddusersad.ps1
+++ bulkaddusersad.ps1
@@ -1,24 +1,30 @@
 ﻿$domain = "YOUR AD DOMAIN HERE"
 $importCsv = Import-Csv adusersimport.csv
 
-Foreach($user in $importCsv){
+Foreach ($user in $importCsv) {
     $fullName = $user.first_name + " " + $user.last_name
-    $formattedName = $user.username+"@$domain"
+    $formattedName = $user.username + "@$domain"
     try{
+        $changePw = [System.Convert]::ToBoolean($user.temp_pw)
+    }
+    catch{
+        Write-Output "Invalid value for temp_pw at user: $formattedName"
+    }
+    try {
         $groupsArr = $user.groups.split(",")
 
-        New-ADUser -name $fullName -GivenName $user.first_name -Surname $user.last_name -UserPrincipalName $formattedName -Enabled $True -AccountPassword (ConvertTo-SecureString $user.password -AsPlainText -Force) -DisplayName $fullName -SamAccountName $user.username -Path $user.ou
-        Write-Host $user.username"user created!"
+        New-ADUser -name $fullName -GivenName $user.first_name -Surname $user.last_name -UserPrincipalName $formattedName -Enabled $True -AccountPassword (ConvertTo-SecureString $user.password -AsPlainText -Force) -DisplayName $fullName -SamAccountName $user.username -Path $user.ou -ChangePasswordAtLogon $changePw
+        Write-Output $user.username"user created!"
 
-        foreach ($group in $groupsArr){
-            try{
-                 Add-ADGroupMember -Identity $group -Members $user.username
-                 Write-Host $user.username"added to"$group"!"
+        foreach ($group in $groupsArr) {
+            try {
+                Add-ADGroupMember -Identity $group -Members $user.username
+                Write-Output $user.username"added to"$group"!"
             }
 
-            catch{
-                    $ErrorMessage = $_.Exception.Message
-                    Write-Output "Issue with adding $user to $group!!! `r`n Error: $ErrorMessage"
+            catch {
+                $ErrorMessage = $_.Exception.Message
+                Write-Output "Issue with adding $user to $group!!! `r`n Error: $ErrorMessage"
             }
         }
     }
